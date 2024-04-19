d380dd12720d3b1e5a7be9374a7c0b7dd686d310
JennyDisc <touki741@hotmail.com>
2024-04-19 20:09:10 +0800

    修正 schema 的 Date.now

--- models/post.js
+++ models/post.js
@@ -31,7 +31,7 @@ const postSchema = new mongoose.Schema(
         },
         createdAt: {
             type: Date,
-            default: Date.now(),
+            default: Date.now,
             // select: false
         },
         likes: {
