c1a95119dc6db6ce09d91bf77f2a95b0349eb70f
JennyDisc <touki741@hotmail.com>
2024-04-20 13:22:58 +0800

    增加刪除全部與 PATCH

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ const requestListener = async (req, res) => {
                     );
                     successHandle(res, newPost);
                 } else {
-                    const message = "欄位未填寫正確，或無此 todo ID";
+                    const message = "欄位未填寫正確，或無該筆貼文 id";
                     errorHandle(res, message);
                 }
             } catch (error) {
@@ -73,15 +73,62 @@ const requestListener = async (req, res) => {
                 errorHandle(res, message);
             };
         })
-    } else if (req.url.startsWith('/posts/') && req.method == 'DELETE') {
-        const id = req.url.split('/').pop();
-        await Post.findByIdAndDelete(id);
+    } else if (req.url == '/posts' && req.method == 'DELETE') {
+        await Post.deleteMany();
         res.writeHead(200, headers);
         res.write(JSON.stringify({
             "status": "success",
-            "data": null
+            "message": "已刪除全部貼文"
         }));
         res.end();
+    } else if (req.url.startsWith('/posts/') && req.method == 'DELETE') {
+        const id = req.url.split('/').pop();
+        const idResult = await Post.findByIdAndDelete(id);
+        // 找到可刪除的會回傳那筆的物件內容。找不到可刪除的則回傳 null
+        console.log(idResult);
+        if (idResult !== null) {
+            res.writeHead(200, headers);
+            res.write(JSON.stringify({
+                "status": "success",
+                "data": null
+            }));
+            res.end();
+        } else {
+            const message = '查無該筆貼文 id';
+            errorHandle(res, message);
+        };
+    } else if (req.url.startsWith('/posts/') && req.method == 'PATCH') {
+        req.on('end', async () => {
+            try {
+                const data = JSON.parse(body);
+                console.log(data);
+                const id = req.url.split("/").pop();
+                const idResult = await Post.findById(id);
+                // 找到這筆 id 會回傳那筆的物件內容。找不到則回傳 null
+                // console.log(idResult);
+                if (data.content !== "" && idResult !== null) {
+                    const newPost = await Post.findByIdAndUpdate(
+                        id,
+                        {
+                            name: data.name,
+                            location: data.location,
+                            type: data.type,
+                            tags: data.tags,
+                            content: data.content,
+                            image: data.image,
+                            likes: data.likes
+                        }
+                    );
+                    successHandle(res, newPost);
+                } else {
+                    const message = '查無該筆貼文內容或 id 屬性';
+                    errorHandle(res, message);
+                };
+            } catch (error) {
+                const message = '查無該筆貼文 id';
+                errorHandle(res, message);
+            };
+        });
     }
     // 預檢請求
     else if (req.url == '/posts' && req.method == 'OPTIONS') {
